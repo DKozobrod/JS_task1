73cf55857e1528e469422edc5c09793dd6c5cfb0
DimaKozobrod <78792194+DKozobrod@users.noreply.github.com>
2021-05-11 15:32:56 +0300

    Update 2_task_array.js

--- 2_task_array.js
+++ 2_task_array.js
@@ -1,4 +1,5 @@
 const arr = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
 
-const flat = arr => arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? flat(val) : val), []);
-console.log(flat(arr));
+const flatten = arr => arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatten(val) : val), []);
+console.log(flatten(arr));
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
