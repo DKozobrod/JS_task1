41d621922deacf1b1071ee128490f20acd0ff2f4
DimaKozobrod <78792194+DKozobrod@users.noreply.github.com>
2021-05-11 11:00:41 +0300

    Update 2_task_array.js

--- 2_task_array.js
+++ 2_task_array.js
@@ -6,3 +6,22 @@ function flatDeep(arr, d = 1) {
 };
 
 console.log(flatDeep(arr, Infinity)); //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+// другой вариант
+var arr1 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
+function flatten(input) {
+  const stack = [...input];
+  const res = [];
+  while (stack.length) {
+    const next = stack.pop();
+    if (Array.isArray(next)) {      
+      stack.push(...next);
+    } else {
+      res.push(next);
+    }
+  }
+  return res.reverse();
+}
+
+console.log(flatten(arr1));
