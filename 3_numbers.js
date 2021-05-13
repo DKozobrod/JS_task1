e25549395570d200ac747e4510ccb48f30e17037
DimaKozobrod <78792194+DKozobrod@users.noreply.github.com>
2021-05-13 12:23:58 +0300

    Update 3_numbers.js

--- 3_numbers.js
+++ 3_numbers.js
@@ -1,64 +1,106 @@
-// let num = prompt('Введи число:', '');
-let arr1 = [
-    '',
-    ' один',
-    ' два',
-    ' три',
-    ' четыре',
-    ' пять',
-    ' шесть',
-    ' семь',
-    ' восемь',
-    ' девять',
-    ' десять',
+"use strict"
+
+function fun(num) {
+	return num.toString().split('').map(Number);
+}
+
+const arrUnit = [
+	' ',
+	' один',
+	' два',
+	' три',
+	' четыре',
+	' пять',
+	' шесть',
+	' семь',
+	' восемь',
+	' девять',
+	' десять',
 ];
-let arr11 = [
-    '',
-    ' одиннадцать',
-    ' двенадцать',
-    ' тринадцать',
-    ' четырнадцать',
-    ' пятнадцать',
-    ' шестнадцать',
-    ' семнадцать',
-    ' восемнадцать',
-    ' девятнадцать',
-    ' двадцать',
+const arrUnit2 = [
+	' десять',
+	' одиннадцать',
+	' двенадцать',
+	' тринадцать',
+	' четырнадцать',
+	' пятнадцать',
+	' шестнадцать',
+	' семнадцать',
+	' восемнадцать',
+	' девятнадцать',
 ];
-let arrDozen = [
-    '',
-    '',
-    ' двадцать',
-    ' тридцать',
-    ' сорок',
-    ' пятьдесят',
-    ' шестьдесят',
-    ' семьдесят',
-    ' восемьдесят',
-    ' девяносто',
+const arrDozen = [
+	'',
+	'',
+	' двадцать',
+	' тридцать',
+	' сорок',
+	' пятьдесят',
+	' шестьдесят',
+	' семьдесят',
+	' восемьдесят',
+	' девяносто',
 ];
-let arrHundred = [
-    '',
-    ' сто',
-    ' двести',
-    ' триста',
-    ' четыреста',
-    ' пятьсот',
-    ' шестьсот',
-    ' семьсот',
-    ' восемьсот',
-    ' девятьсот',
+const arrDozen1 = [
+	'',
+	'',
+	' двадцать тысяч',
+	' тридцать тысяч',
+	' сорок тысяч',
+	' пятьдесят тысяч',
+	' шестьдесят тысяч',
+	' семьдесят тысяч',
+	' восемьдесят тысяч',
+	' девяносто тысяч',
 ];
-let arrThousand = (' тысяч', ' одна тысяча', ' две тысячи');
+const arrHundred = [
+	'',
+	' сто',
+	' двести',
+	' триста',
+	' четыреста',
+	' пятьсот',
+	' шестьсот',
+	' семьсот',
+	' восемьсот',
+	' девятьсот',
+];
+const arrThousand = [
+	'',
+	' одна тысяча',
+	' две тысячи',
+	' три тысячи',
+	' четыре тысячи',
+	' пять тысяч',
+	' шесть тысяч',
+	' семь тысяч',
+	' восемь тысяч',
+	' девять тысяч',
+];
 
 const foo = (num) => {
-    if (num <= 10) {
-        console.log(arr1[num]);
-    } else if (num / 10 < 2) {
-        console.log(arr11[num % 10]);
-    } else if (num / 10 < 10) {
-        console.log(arrDozen[Math.floor(num / 10)] + arr1[num - 20]);
-    }
-};
+	const mas = fun(num)
 
+mas.reverse() 
+	
+const var0 = mas[0]; //единицы
+const var1 = mas[1]; //десятки
+const var2 = mas[2]; //сотни
+const var3 = mas[3]; //тысячи
+const var4 = mas[4]; //десятки тысяч
+const var5 = mas[5]; //сотни тысяч
+
+if (var4 === 1 && var1 != 1) {
+	console.log(arrHundred[var5]+''+ arrUnit2[var4, var3]+' тысяч'+arrHundred[var2]+''+arrDozen[var1]+''+arrUnit[var0]);
+} else if (var1 === 1 && var4 != 1) {
+	console.log(arrHundred[var5]+''+arrDozen[var4]+''+arrThousand[var3]+''+arrHundred[var2]+''+arrUnit2[var1, var0]);
+} else if ((var4 === 1) && (var1 === 1)) {
+	console.log(arrHundred[var5]+''+arrUnit2[var4, var3]+' тысяч'+arrHundred[var2]+''+arrUnit2[var1, var0]);
+} else if (var3 === 0 || var4 ===0) {
+	console.log(arrHundred[var5]+''+ arrDozen[var4]+' тысяч'+arrThousand[var3]+''+arrHundred[var2]+''+arrDozen[var1]+''+arrUnit[var0]);
+} else {
+	console.log(arrHundred[var5]+''+ arrDozen[var4]+''+arrThousand[var3]+''+arrHundred[var2]+''+arrDozen[var1]+''+arrUnit[var0]);
+}
+
+};
 window.myFoo = foo;
