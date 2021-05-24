bab3d4ee174693b637bf83f018c32593605fa2d4
DimaKozobrod <78792194+DKozobrod@users.noreply.github.com>
2021-05-24 16:22:19 +0300

    Update 4_compare_array_elements.js

--- 4_compare_array_elements.js
+++ 4_compare_array_elements.js
@@ -4,18 +4,18 @@
 // то true. Например ['asd', 'afffd', 'cc', 'kk']. 
 // Для такого массива функция должна вернуть [true, false, false]
 
-let array = ['asd', 'afffd', 'cc', 'kk'];
+let array = ['asd', 'afffd', 'accd', 'kk'];
 function checkArray() {
 let arr = new Array();
 
 for (let i = 0; i < array.length - 1; i++) {
 
-    let a = array[i][0];
-    let b = array[i][array[i].length - 1];
-    let c = array[i+1][0];
-    let d = array[i+1][array[i+1].length - 1];
+    let firstValueCurrent = array[i][0];
+    let lastValueCurrent = array[i][array[i].length - 1];
+    let firstValueNext = array[i+1][0];
+    let lastValueNext = array[i+1][array[i+1].length - 1];
 
-  if ((a + b) == (c + d)) {
+  if ((firstValueCurrent + lastValueCurrent) == (firstValueNext + lastValueNext)) {
         arr.push("true");
     } else {
         arr.push("false");
