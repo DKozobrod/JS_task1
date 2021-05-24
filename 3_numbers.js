93113e537f07a589b2acb52c08f31463e4842314
DimaKozobrod <78792194+DKozobrod@users.noreply.github.com>
2021-05-24 19:57:51 +0300

    Update 3_numbers.js

--- 3_numbers.js
+++ 3_numbers.js
@@ -6,83 +6,83 @@ function fun(num) {
 
 const arrUnit = [
 	' ',
-	' один',
-	' два',
-	' три',
-	' четыре',
-	' пять',
-	' шесть',
-	' семь',
-	' восемь',
-	' девять',
-	' десять',
+	'один',
+	'два',
+	'три',
+	'четыре',
+	'пять',
+	'шесть',
+	'семь',
+	'восемь',
+	'девять',
+	'десять',
 ];
 const arrUnit2 = [
-	' десять',
-	' одиннадцать',
-	' двенадцать',
-	' тринадцать',
-	' четырнадцать',
-	' пятнадцать',
-	' шестнадцать',
-	' семнадцать',
-	' восемнадцать',
-	' девятнадцать',
+	'десять',
+	'одиннадцать',
+	'двенадцать',
+	'тринадцать',
+	'четырнадцать',
+	'пятнадцать',
+	'шестнадцать',
+	'семнадцать',
+	'восемнадцать',
+	'девятнадцать',
 ];
 const arrDozen = [
 	'',
 	'',
-	' двадцать',
-	' тридцать',
-	' сорок',
-	' пятьдесят',
-	' шестьдесят',
-	' семьдесят',
-	' восемьдесят',
-	' девяносто',
+	'двадцать',
+	'тридцать',
+	'сорок',
+	'пятьдесят',
+	'шестьдесят',
+	'семьдесят',
+	'восемьдесят',
+	'девяносто',
 ];
 const arrDozen1 = [
 	'',
 	'',
-	' двадцать тысяч',
-	' тридцать тысяч',
-	' сорок тысяч',
-	' пятьдесят тысяч',
-	' шестьдесят тысяч',
-	' семьдесят тысяч',
-	' восемьдесят тысяч',
-	' девяносто тысяч',
+	'двадцать тысяч',
+	'тридцать тысяч',
+	'сорок тысяч',
+	'пятьдесят тысяч',
+	'шестьдесят тысяч',
+	'семьдесят тысяч',
+	'восемьдесят тысяч',
+	'девяносто тысяч',
 ];
 const arrHundred = [
 	'',
-	' сто',
-	' двести',
-	' триста',
-	' четыреста',
-	' пятьсот',
-	' шестьсот',
-	' семьсот',
-	' восемьсот',
-	' девятьсот',
+	'сто',
+	'двести',
+	'триста',
+	'четыреста',
+	'пятьсот',
+	'шестьсот',
+	'семьсот',
+	'восемьсот',
+	'девятьсот',
 ];
 const arrThousand = [
 	'',
-	' одна тысяча',
-	' две тысячи',
-	' три тысячи',
-	' четыре тысячи',
-	' пять тысяч',
-	' шесть тысяч',
-	' семь тысяч',
-	' восемь тысяч',
-	' девять тысяч',
+	'одна тысяча',
+	'две тысячи',
+	'три тысячи',
+	'четыре тысячи',
+	'пять тысяч',
+	'шесть тысяч',
+	'семь тысяч',
+	'восемь тысяч',
+	'девять тысяч',
 ];
 
 const foo = (num) => {
 	const mas = fun(num)
 
 mas.reverse() 
-	
+
 const var0 = mas[0]; //единицы
 const var1 = mas[1]; //десятки
 const var2 = mas[2]; //сотни
@@ -90,17 +90,30 @@ const var3 = mas[3]; //тысячи
 const var4 = mas[4]; //десятки тысяч
 const var5 = mas[5]; //сотни тысяч
 
-if (var4 === 1 && var1 != 1) {
-	console.log(arrHundred[var5]+''+ arrUnit2[var4, var3]+' тысяч'+arrHundred[var2]+''+arrDozen[var1]+''+arrUnit[var0]);
-} else if (var1 === 1 && var4 != 1) {
-	console.log(arrHundred[var5]+''+arrDozen[var4]+''+arrThousand[var3]+''+arrHundred[var2]+''+arrUnit2[var1, var0]);
-} else if ((var4 === 1) && (var1 === 1)) {
-	console.log(arrHundred[var5]+''+arrUnit2[var4, var3]+' тысяч'+arrHundred[var2]+''+arrUnit2[var1, var0]);
-} else if (var3 === 0 || var4 ===0) {
-	console.log(arrHundred[var5]+''+ arrDozen[var4]+' тысяч'+arrThousand[var3]+''+arrHundred[var2]+''+arrDozen[var1]+''+arrUnit[var0]);
-} else {
-	console.log(arrHundred[var5]+''+ arrDozen[var4]+''+arrThousand[var3]+''+arrHundred[var2]+''+arrDozen[var1]+''+arrUnit[var0]);
-}
+const sixElem = (arrHundred[var5] || '' );
+const fiveElem = (arrDozen[var4] || '');
+const five11Elem = (arrUnit2[var4, var3] || '');
+const fourElem = (arrHundred[var2] || '');
+const threeElem = (arrThousand[var3] || '');
+const twoElem = (arrDozen[var1] || '');
+const oneElem = (arrUnit[var0] || '');
+const elevenElem = (arrUnit2[var1, var0] || '');
 
+let result = '';
+
+if (var4 === 1 && var1 != 1) {
+	result = `${sixElem} ${five11Elem} тысяч ${fourElem} ${twoElem} ${oneElem}`;
+} else if (var1 === 1 && var4 != 1) {
+	result = `${sixElem} ${fiveElem} ${threeElem} ${fourElem} ${elevenElem}`;
+} else if ((var4 === 1) && (var1 === 1)) {
+	result = `${sixElem} ${five11Elem} тысяч ${fourElem} ${elevenElem}`;
+} else if (var3 === 0 || var4 === 0) {
+	result = `${sixElem} ${fiveElem} тысяч ${threeElem} ${fourElem} ${twoElem} ${oneElem}`;
+} else { 
+    result = `${sixElem} ${fiveElem} ${threeElem} ${fourElem} ${twoElem} ${oneElem}`;
+}
+return result.trim();
 };
-window.myFoo = foo;
+
+
+console.log(foo(12));
